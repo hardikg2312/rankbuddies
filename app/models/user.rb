4cf3748942fcb9ddf9126d6eabf72399f8469f7f
Hardik <hardikg23@gmail.com>
2014-10-24 20:39:31 +0530

    did : user_name added in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ActiveRecord::Base
   attr_accessor :password
   before_save :encrypt_password
   
-  validates_presence_of :email
+  validates_presence_of   :email
   validates_uniqueness_of :email
+  validates_length_of     :email,    :within => 3..100
+  validates_format_of     :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_presence_of   :user_name
+  validates_uniqueness_of :user_name
+  validates_length_of     :user_name,    :within => 3..40
   validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
+  validates_presence_of   :password, :on => :create
+  validates_length_of     :password,    :within => 5..40
 
   def self.authenticate(email, password)
-    user = find_by_email(email)
+    user = find_by_email(email) || find_by_user_name(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023180446) do
+ActiveRecord::Schema.define(version: 20141024145447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141023180446) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_name"
   end
 
 end
