cee84448f56f4794cfba3af6895e43328329cd10
Hardik <hardikg23@gmail.com>
2014-10-24 20:18:53 +0530

    did : login and sign up validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
+  def create  # will render create.js.erb
+    @user = User.authenticate(params[:email], params[:password])
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url, :flash => { :success => "Logged out!" } 
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  def index 
   end
 
-  def create
-  	user_params
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+  def new
+    if current_user
+      redirect_to users_path
     else
-      render "new"
+      @user = User.new
+      render :layout => false
     end
+  end
+
+  def create # will render create.js.erb
+    @user = User.new(user_params)
   end
 
   private
